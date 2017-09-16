f86751d43532408310ee8daaf0b5f1c90ceec60f
samuel orji <awesomeorji@gmail.com>
2017-09-16 21:39:07 +0100

    Profile activity bootom nav bar and tool bar setup

--- app/src/main/java/com/example/samuel/sawft/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/samuel/sawft/Profile/ProfileActivity.java
@@ -2,6 +2,8 @@ package com.example.samuel.sawft.Profile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,12 +14,44 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 public class ProfileActivity extends AppCompatActivity {
 
     private static final int ACTIVITY_NUMBER = 4;
+    private static final String TAG = "ProfileActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         setUpBottomNavBar();
+        setUpToolbar();
+
+    }
+
+   public void setUpToolbar(){
+       Toolbar bar = (Toolbar) findViewById(R.id.profile_toolbar);
+       setSupportActionBar(bar);
+       bar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+           @Override
+           public boolean onMenuItemClick(MenuItem item) {
+
+               switch (item.getItemId()){
+                   case R.id.profile_Activity_menu:
+                       Log.d(TAG, "onMenuItemClick: profile menu clicked");
+
+                       break;
+
+               }
+               return true;
+           }
+       });
+
+
+
+
+   }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,menu);
+        return true;
     }
 
     public void setUpBottomNavBar(){
